a96c3f69df8c04bf55c42ed1cffcf67a4e904c60
stephenarthur64 <c00295678@setu.ie>
2025-02-20 15:02:55 +0000

    Swarmer hovering

--- GamesFleadh/GamesFleadh/include/Swarmer.h
+++ GamesFleadh/GamesFleadh/include/Swarmer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "gameobject.h"
 #include "IdleState.h"
+#include "Globals.h"
 
 class Swarmer :
     public GameObject
@@ -12,10 +13,19 @@ public:
 	virtual void init();
 	virtual void render();
 
+	void setLimits(float t_upperLimit, float t_lowerLimit);
+
 	void update();
 	void playerSpotted(bool t_spotted);
+	void hover();
 
 private:
 	int spottedTick = 0;
+
+	float m_upperLimit;
+	float m_lowerLimit;
+
+	float m_speed;
+	Direction m_direction;
 };
 
--- GamesFleadh/GamesFleadh/src/Swarmer.cpp
+++ GamesFleadh/GamesFleadh/src/Swarmer.cpp
@@ -1,6 +1,6 @@
 #include "Swarmer.h"
 
-Swarmer::Swarmer()
+Swarmer::Swarmer() : m_speed(0.1f), m_direction(NORTH)
 {
 	currentState = new IdleState;
 	m_position = { -2.0f, 1.0f, -12.0f };
@@ -16,11 +16,18 @@ void Swarmer::rotate(int t_direction)
 void Swarmer::init()
 {
 	m_body = LoadModel("ASSETS/3D/Enemy/Swarmer/Swarmer.glb");
+	setLimits(5, 0);
 }
 
 void Swarmer::render()
 {
 	DrawModel(m_body, m_position, 1.0f, WHITE);
+}
+
+void Swarmer::setLimits(float t_upperLimit, float t_lowerLimit)
+{
+	m_upperLimit = t_upperLimit;
+	m_lowerLimit = t_lowerLimit;
 }
 
 void Swarmer::update()
@@ -33,7 +40,9 @@ void Swarmer::update()
 	else
 	{
 		playerSpotted(false);
+		hover();
 	}
+
 }
 
 void Swarmer::playerSpotted(bool t_spotted)
@@ -47,5 +56,28 @@ void Swarmer::playerSpotted(bool t_spotted)
 	{
 		spottedTick = -1;
 		handleInput(Event::EVENT_NONE);
+	}
+}
+
+void Swarmer::hover()
+{
+	if (m_direction == NORTH)
+	{
+		m_position.y += m_speed;
+
+		if (m_upperLimit < m_position.y)
+		{
+			m_direction = SOUTH;
+		}
+	}
+
+	if (m_direction == SOUTH)
+	{
+		m_position.y -= m_speed;
+
+		if (m_lowerLimit > m_position.y)
+		{
+			m_direction = NORTH;
+		}
 	}
 }
